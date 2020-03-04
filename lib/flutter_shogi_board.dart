871cb02ca548bcaf6d699680e84f8c998ab7042d
James Leahy <defuncart@gmail.com>
2020-03-04 18:35:28 +0100

    Update export file.

--- lib/flutter_shogi_board.dart
+++ lib/flutter_shogi_board.dart
@@ -1,4 +1,5 @@
 library flutter_shogi_board;
 
+export 'src/configs/board_colors.dart';
 export 'src/widgets/shogi_board.dart';
 export 'src/widgets/coord_indicator_cell.dart' show CoordIndicatorType;
