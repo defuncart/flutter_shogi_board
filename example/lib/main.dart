6a8d37bc4ebe681125b4605c65f746c333dec42e
James Leahy <defuncart@gmail.com>
2020-03-11 17:08:04 +0100

    Add proverb example.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,6 +14,7 @@ class _HomeScreen extends StatelessWidget {
   final Map<String, Function(BuildContext)> routes = {
     'Yagura castle building animation': (context) => _showPage(context, _CastleBuildingAnimation()),
     'Tsume (5手詰)': (context) => _showPage(context, _Tsume()),
+    'Proverb': (context) => _showPage(context, _Proverb()),
   };
 
   static void _showPage(BuildContext context, Widget page) =>
@@ -156,6 +157,148 @@ class _Tsume extends StatelessWidget {
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class _Proverb extends StatelessWidget {
+  const _Proverb({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Avoid a Sitting King'),
+      ),
+      body: DefaultShogiBoardStyle(
+        style: ShogiBoardStyle(
+          maxSize: 400,
+          coordIndicatorType: CoordIndicatorType.arabic,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Container(height: 16),
+                Text(
+                    'It is extremely dangerous to start fighting with the King sitting on the original square. In Diagram 1, Black has already advanced a Silver onto 4f with his King in the original position. If he wants to launch an attack from here, how would he play?'),
+                Container(height: 16),
+                _SFENBoard(
+                  sfenString: 'ln3k1nl/1r3bg2/p1ppsgspp/1p2ppp2/7P1/2PPPSP2/PPS2P2P/2G1G2R1/LNB1k2NL b -',
+                  label: 'Diagram 1',
+                  // showPiecesInHand: false,
+                ),
+                Container(height: 16),
+                _MovesList(
+                  moves: [
+                    'P-3e',
+                    'Px3e',
+                    'Sx3e',
+                    'P*3d',
+                    'P-2d',
+                    'Px2d',
+                    'Sx2d',
+                    'Sx2d',
+                    'Bx2d',
+                    'Bx2d',
+                    'Rx2d',
+                  ],
+                  playerFirstMove: PlayerType.sente,
+                ),
+                Container(height: 16),
+                _SFENBoard(
+                  sfenString: 'ln3k1nl/1r4g2/p1ppsg2p/1p2pppR1/9/2PPP4/PPS2P2P/2G1G4/LN2k2NL b bspBS2P',
+                  label: 'Diagram 2',
+                  showPiecesInHand: true,
+                ),
+                Container(height: 16),
+                Text(
+                  '''So far, Black's climbing Silver appears to have made a point. But White has a devastating move to play here.''',
+                ),
+                Container(height: 8),
+                _MovesList(
+                  moves: [
+                    'B*1e',
+                  ],
+                  playerFirstMove: PlayerType.gote,
+                ),
+                Container(height: 8),
+                Text(
+                    'You cannot be too careful when you have a sitting King. Black had to play K6i first in this case. Then the attack would have been successful.'),
+                Container(height: 8),
+                Text(
+                  'Content taken from http://www.shogi.net/kakugen/.',
+                  style: TextStyle(
+                    fontSize: 10,
+                  ),
+                ),
+                Container(height: 8),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _SFENBoard extends StatelessWidget {
+  final String sfenString;
+  final String label;
+  final bool showPiecesInHand;
+
+  const _SFENBoard({
+    @required this.sfenString,
+    this.label,
+    this.showPiecesInHand = true,
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          ShogiBoard(
+            gameBoard: ShogiUtils.sfenStringToGameBoard(sfenString),
+            showPiecesInHand: showPiecesInHand,
+          ),
+          if (label != null)
+            Column(
+              children: <Widget>[
+                if (!showPiecesInHand) Container(height: 4),
+                Text(label),
+              ],
+            ),
+        ],
+      ),
+    );
+  }
+}
+
+class _MovesList extends StatelessWidget {
+  final List<String> moves;
+  final PlayerType playerFirstMove;
+
+  const _MovesList({
+    @required this.moves,
+    this.playerFirstMove = PlayerType.sente,
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Wrap(
+        spacing: 4.0,
+        runSpacing: 4.0,
+        children: <Widget>[
+          for (int i = 0; i < moves.length; i++)
+            Text((i % 2 == 0 && playerFirstMove == PlayerType.sente ? BoardConfig.sente : BoardConfig.gote) + moves[i]),
+        ],
       ),
     );
   }
