b674f10a4c2faa6f5f5e1666ff44a4a36e8da346
James Leahy <defuncart@gmail.com>
2021-02-07 12:44:30 +0100

    Add PlayerIcon.

---
+++ lib/src/widgets/player_icon.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:shogi/shogi.dart';
+
+import 'autosize_text.dart';
+
+/// Renders a player's icon
+class PlayerIcon extends StatelessWidget {
+  /// A multiplier to render the icon smaller than for a piece
+  static const _sizeMultiplier = 0.8;
+
+  /// Whether the piece belongs to sente (facing upwards)
+  final bool isSente;
+
+  /// The cell's size (width, height)
+  final double size;
+
+  /// The color of the icon
+  final Color color;
+
+  const PlayerIcon({
+    Key key,
+    @required this.isSente,
+    @required this.size,
+    @required this.color,
+  })  : assert(isSente != null),
+        assert(size != null && size > 0),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AutosizeText(
+      isSente ? BoardConfig.sente : BoardConfig.gote,
+      textDirection: isSente ? TextDirection.upwards : TextDirection.downwards,
+      size: size,
+      fontSizeMultiplier: _sizeMultiplier,
+      color: color,
+    );
+  }
+}
